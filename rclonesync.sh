07e16fee9077e49b4b6e94a1ffd7186122c22d9c
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2022-08-26 19:16:50 -0400

    Update rclonesync.sh

--- rclonesync.sh
+++ rclonesync.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 mkdir ~/.config
 mkdir /data/config
-mkdir /data/sync
+mkdir /data/sync/shared-input
+mkdir /data/sync/shared-output
 ln -s /data/config ~/.config/rclone
 
 if [ ! -f "$HOME/.config/rclone/rclone.conf" ]; then
   echo Please run: rclone config create dropbox dropbox
   echo And place file in /mnt/integrity_store/store/rclone/config/rclone.conf
   sleep 10000
-  exit 0
 fi
 
+#rclone config create dropbox dropbox
 while [ 1 ]
 do
-  rclone copy /data/sync dropbox:$DROPBOX_PATH --create-empty-src-dirs; \
-  rclone copy dropbox:$DROPBOX_PATH /data/sync --create-empty-src-dirs
-  sleep 60
+  time rclone copy /data/sync/shared-output dropbox:$DROPBOX_PATH_OUTPUT --create-empty-src-dirs
+  touch /data/sync/shared-input/.rclone_lock
+  time rclone copy dropbox:$DROPBOX_PATH_INPUT /data/sync/shared-input --create-empty-src-dirs
+  rm -rf /data/sync/shared-input/.rclone_lock
+  sleep 30
 done
