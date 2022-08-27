fef8621beb3e0fe7444dc863f7c27ae144ca7060
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2022-08-26 21:20:34 -0400

    Update rclonesync.sh

--- rclonesync.sh
+++ rclonesync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir ~/.config
+mkdir $HOME/.config
 mkdir /data/config
 mkdir /data/sync/shared-input
 mkdir /data/sync/shared-output
-ln -s /data/config ~/.config/rclone
+ln -s /data/config $HOME/.config/rclone
 
 if [ ! -f "$HOME/.config/rclone/rclone.conf" ]; then
   echo Please run: rclone config create dropbox dropbox
@@ -14,9 +14,9 @@ fi
 #rclone config create dropbox dropbox
 while [ 1 ]
 do
-  time rclone copy /data/sync/shared-output dropbox:$DROPBOX_PATH_OUTPUT --create-empty-src-dirs
+  rclone copy /data/sync/shared-output dropbox:$DROPBOX_PATH_OUTPUT --create-empty-src-dirs
   touch /data/sync/shared-input/.rclone_lock
-  time rclone copy dropbox:$DROPBOX_PATH_INPUT /data/sync/shared-input --create-empty-src-dirs
+  rclone copy dropbox:$DROPBOX_PATH_INPUT /data/sync/shared-input --create-empty-src-dirs
   rm -rf /data/sync/shared-input/.rclone_lock
   sleep 30
 done
